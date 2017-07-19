80b7d25faf5de5c826dfa58ec20fcec3b893cc00
xfredlix <fred.m.li.93@gmail.com>
2017-07-18 17:31:13 -0700

    took out redux logger

--- store.js
+++ store.js
@@ -1,6 +1,5 @@
 import { applyMiddleware, createStore, compose} from "redux"
 
-import {createLogger} from "redux-logger"
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
 
